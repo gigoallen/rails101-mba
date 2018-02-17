ac16dbd2d23b18a3364c56cde94460ced5e7af44
GigoAllen <gigo.allen@gmail.com>
2018-02-17 12:05:36 +0800

    user before_action to find_group_and_check_permission

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,6 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
 
   def index
     @groups = Group.all
@@ -24,17 +25,9 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id])
-    if current_user != @group.user
-      redirect_to root_path, alert: "沒有權限修改"
-    end
   end
 
   def update
-    @group = Group.find(params[:id])
-    if current_user != @group.user
-      redirect_to root_path, alert: "沒有權限修改"
-    end
     if @group.update(group_params)
       flash[:success] = "Update success"
       redirect_to groups_path, notice: "Update success 更新成功了"
@@ -44,10 +37,6 @@ class GroupsController < ApplicationController
   end
 
   def destroy
-    @group = Group.find(params[:id])
-    if current_user != @group.user
-      redirect_to root_path, alert: "沒有權限修改"
-    end
     if @group.destroy
       flash[:alert] = "Group deleted"
       redirect_to groups_path, notice: "刪除成功了！"
@@ -59,5 +48,12 @@ class GroupsController < ApplicationController
   private
   def group_params
     params.require(:group).permit(:title, :description)
+  end
+
+  def find_group_and_check_permission
+    @group = Group.find(params[:id])
+    if current_user != @group.user
+      redirect_to root_path, alert: "沒有權限修改"
+    end
   end
 end
