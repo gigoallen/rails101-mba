e3bdcccc03391f6069cd72b6be43902c2042dc11
GigoAllen <gigo.allen@gmail.com>
2018-02-17 16:35:09 +0800

    implement is_member_of?(group)

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,9 @@
 class Group < ApplicationRecord
   belongs_to :user
   has_many :posts
+  #以下為多對多的設定
+  has_many :group_user_relationships
+  has_many :members, through: :group_user_relationships, source: :user
   validates :title, presence: true
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,9 @@ class User < ApplicationRecord
   #以下為多對多的設定
   has_many :group_user_relationships
   has_many :participated_groups, through: :group_user_relationships, source: :group
+
+  #判斷成員是否為group中的一員
+  def is_member_of?(group)
+    participated_groups.include?(group)
+  end
 end
